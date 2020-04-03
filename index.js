e7722aab758de40205044606b89628e81ca28b90
readme <readme55@protonmail.com>
2020-04-03 19:47:43 +0200

    update for dapphack

--- index.js
+++ index.js
@@ -1,38 +1,62 @@
-const clientOpts = {
-  network: 'testnet',
-  mnemonic: 'toddler repair print phrase crouch curve charge typical swap bachelor outer upgrade'
-};
-const client = new Dash.Client(clientOpts);
+let submitBtn = document.getElementById('submitBtn');
+let submitText = document.getElementById('submitText');
+// let submitText = document.getElementById('submitText');
+// let submitText = document.getElementById('submitText');
+// let submitText = document.getElementById('submitText');
 
-async function connect() {
-  try {
-    await client.isReady();
-    console.log('connected');
-  } catch (e) {
-    console.error('Something went wrong:', e);
-  } finally {
-    client.disconnect();
-  }
-}
+// external registered user Identity DUxf95cCdPTor7BfWMXmr2VmHQqdKMPQv6fauecy7Wuy
 
-connect();
+submitBtn.addEventListener('click', function () {
+  console.log("click")
+  submitBtn.disabled = true;
 
-// external registered Identity DUxf95cCdPTor7BfWMXmr2VmHQqdKMPQv6fauecy7Wuy
+  const clientOpts = {
+    network: 'testnet',
+    mnemonic: 'toddler repair print phrase crouch curve charge typical swap bachelor outer upgrade',
+    apps: {
+      loginContract: {
+        contractId: '7kXTykyrTW192bCTKiMuEX2s15KExZaHKos8GrWCF21D'
+      }
+    }  
+  };
+  const client = new Dash.Client(clientOpts);
+  
+  const submitNoteDocument = async function () {
 
-const registerName = async function () {
-  try {
-    await client.isReady();
     const platform = client.platform;
-    const identity = await platform.identities.get('DUxf95cCdPTor7BfWMXmr2VmHQqdKMPQv6fauecy7Wuy');
-    const nameRegistration = await platform.names.register('dappuser', identity);
-    console.log({nameRegistration});
-  } catch (e) {
-    console.error('Something went wrong:', e);
-  } finally {
-    client.disconnect();
-  }
-}
+    await client.isReady();
+  
+    try {
+      const identity = await platform.identities.get('DUxf95cCdPTor7BfWMXmr2VmHQqdKMPQv6fauecy7Wuy');
 
-registerName();
+      docProperties = {
+        message: submitText.value + ' ' + new Date().toUTCString()
+      }
+      // Create the note document
+      const noteDocument = await platform.documents.create(
+        'loginContract.login',
+        identity,
+        docProperties,
+      );
+      // Sign and submit the document
+      await platform.documents.broadcast(noteDocument, identity);
+    } catch (e) {
+      console.error('Something went wrong:', e);
+    } finally {
+      console.log("submited login document with message: " + submitText.value)
+      client.disconnect();
+    }
+  };
+  submitNoteDocument();
+  submitBtn.disabled = false;
+  console.log("done")
+
+}, false);
+
+
+
+
+
+
 
 
