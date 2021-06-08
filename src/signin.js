db90d54698365a12630ba8d69713e383788051c1
readme <readme55@protonmail.com>
2021-06-08 13:28:11 +0200

    add dapi addresses

--- src/signin.js
+++ src/signin.js
@@ -24,6 +24,11 @@ $(document).ready(function () {
         // Submit a document ("Request Document ST") to the Users Wallet
         clientOpts = {};
         clientOpts.network = dashNetwork;
+        clientOpts.dapiAddresses = [
+            '34.220.41.134',
+            '18.236.216.191',
+            '54.191.227.118',
+        ];
         clientOpts.wallet = {};
         clientOpts.wallet.mnemonic = dappMnemonic;
         clientOpts.wallet.adapter = new LocalForageWrapper();
